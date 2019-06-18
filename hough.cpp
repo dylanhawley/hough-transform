5abcf635066ae5d614f424bedcdaead5f8a8715b
Dylan Hawley <dylanthomashawley@gmail.com>
2019-06-18 12:11:06 -0400

    adjusted hough parameters and added terminal output

--- hough.cpp
+++ hough.cpp
@@ -1,11 +1,14 @@
+#include <iostream>
+
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 using namespace cv;
 using namespace std;
+
 int main(int argc, char** argv) {
   // Declare the output variables
-  Mat dst, cdst, cdstP;
+  Mat dst, cdst;
   const char* default_file = "../cuberover-teleops/localization/data/render_set_5/0degrees.jpg";
   const char* filename = argc >=2 ? argv[1] : default_file;
   // Loads an image
@@ -18,12 +21,13 @@ int main(int argc, char** argv) {
   }
   // Edge detection
   Canny(src, dst, 350, 900, 3);
+  imshow("Canny", dst);
   // Copy edges to the images that will display the results in BGR
   cvtColor(dst, cdst, COLOR_GRAY2BGR);
-  cdstP = cdst.clone();
+  Mat cdstP(cdst.rows, cdst.cols, CV_8UC3, Scalar(0,0, 0));
   // Standard Hough Line Transform
   vector<Vec2f> lines; // will hold the results of the detection
-  HoughLines(dst, lines, 1, CV_PI/180, 750, 0, 0 ); // runs the actual detection
+  HoughLines(dst, lines, 1, CV_PI/180, 40, 0, 0 ); // runs the actual detection
   // Draw the lines
   for (size_t i = 0; i < lines.size(); i++) {
     float rho = lines[i][0], theta = lines[i][1];
@@ -35,18 +39,20 @@ int main(int argc, char** argv) {
     pt2.x = cvRound(x0 - 1000*(-b));
     pt2.y = cvRound(y0 - 1000*(a));
     line( cdst, pt1, pt2, Scalar(0,0,255), 3, LINE_AA);
+    cout << "* line " << i << ": (" << pt1.x << ", " << pt1.y << "), (";
+    cout << pt2.x << ", " << pt2.y << ")" << endl;
   }
   // Probabilistic Line Transform
   vector<Vec4i> linesP; // will hold the results of the detection
-  HoughLinesP(dst, linesP, 1, CV_PI/180, 50, 50, 10 ); // runs the actual detection
+  HoughLinesP(dst, linesP, 1, CV_PI/180, 10, 50, 10 ); // runs the actual detection
   // Draw the lines
   for (size_t i = 0; i < linesP.size(); i++) {
     Vec4i l = linesP[i];
-    line(cdstP, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, LINE_AA);
+    line(cdstP, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255,0,0), 1, LINE_AA);
   }
   // Show results
-  imshow("Source", src);
-  imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst);
+  // imshow("Source", src);
+  // imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst);
   imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP);
   // Wait and Exit
   waitKey();
